a4edc984c2117395e670a8b48663e506e94d6223
Patrick Plenefisch <simonpatp@gmail.com>
2014-09-04 18:15:15 -0400

    header getter script

---
+++ get_linux.sh
@@ -0,0 +1,9 @@
+mkdir headers
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git
+cd linux-stable-rt
+git checkout v3.2.35-rt52
+make INSTALL_HDR_PATH=$PWD/../usr headers_install
+find $PWD/../usr/include \( -name .install -o -name ..install.cmd \) -delete
+cd ..
+tar cjf linux-headers.tar.bz2 usr
+
