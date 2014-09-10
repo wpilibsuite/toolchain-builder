7a1e1fc4004683435a9f7a8e4dd62d623b8e7304
Patrick Plenefisch <simonpatp@gmail.com>
2014-09-10 13:13:06 -0400

    get the ARM headers

--- get_linux.sh
+++ get_linux.sh
@@ -2,7 +2,7 @@ mkdir headers
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git
 cd linux-stable-rt
 git checkout v3.2.35-rt52
-make INSTALL_HDR_PATH=$PWD/../usr headers_install
+make INSTALL_HDR_PATH=$PWD/../usr ARCH=arm headers_install
 find $PWD/../usr/include \( -name .install -o -name ..install.cmd \) -delete
 cd ..
 tar cjf linux-headers.tar.bz2 usr
