bb83d03e1e1d3049c6d06a723d06bf03c897dd95
Peter Johnson <johnson.peter@gmail.com>
2018-10-08 21:34:33 -0700

    Pull libgcc and libstdc++ from upstream NI distribution (#23)
    Also include debug libs in dev packages.

--- download.sh
+++ download.sh
@@ -11,7 +11,23 @@ wget -nc -nv \
     http://isl.gforge.inria.fr/isl-${V_ISL}.tar.bz2 \
     https://ftp.gnu.org/gnu/gdb/gdb-${V_GDB}.tar.gz \
     https://sourceforge.net/projects/expat/files/expat/${Vw_EXPAT}/expat-${Vw_EXPAT}.tar.bz2 \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/gcc_${Va_GCC}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libgcc1_${Va_LIBGCC}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libgcc-s-dbg_${Va_LIBGCC}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libgcc-s-dev_${Va_LIBGCC}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libstdc++6_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libstdc++-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libatomic1_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libatomic-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libgomp1_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libgomp-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libitm1_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libitm-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libssp0_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libssp-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/gcc-runtime-dbg_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
     http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libc6_${Va_LIBC}_cortexa9-vfpv3.ipk \
+    http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libc6-dbg_${Va_LIBC}_cortexa9-vfpv3.ipk \
     http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libc6-dev_${Va_LIBC}_cortexa9-vfpv3.ipk \
     http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/linux-libc-headers-dev_${Va_LINUX}_cortexa9-vfpv3.ipk \
     http://download.ni.com/ni-linux-rt/feeds/2018/arm/cortexa9-vfpv3/libcidn1_${Va_LIBC}_cortexa9-vfpv3.ipk \
--- repack.sh
+++ repack.sh
@@ -13,13 +13,33 @@ then
 	alias tar=gtar
 fi
 
-mkdir -p repack/{libc6,libc6-dev,linux-libc-headers-dev}/out
+mkdir -p repack/{libc6,libc6-dev,linux-libc-headers-dev,libgcc1,libgcc-dev,libstdc\+\+6,libstdc\+\+6-dev}/out
 mv linux-libc-headers-dev_${Va_LINUX}_cortexa9-vfpv3.ipk repack/linux-libc-headers-dev/
 mv libc6_${Va_LIBC}_cortexa9-vfpv3.ipk repack/libc6/
-# the rest are in dev
+mv libgcc1_${Va_LIBGCC}_cortexa9-vfpv3.ipk repack/libgcc1/
+mv \
+    libgcc-s-dbg_${Va_LIBGCC}_cortexa9-vfpv3.ipk \
+    libgcc-s-dev_${Va_LIBGCC}_cortexa9-vfpv3.ipk \
+    repack/libgcc-dev/
+mv \
+    libstdc++6_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libatomic1_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libgomp1_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libitm1_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libssp0_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    repack/libstdc++6/
+mv \
+    libstdc++-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libatomic-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libgomp-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libitm-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    libssp-dev_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    gcc-runtime-dbg_${Va_LIBSTDCPP}_cortexa9-vfpv3.ipk \
+    repack/libstdc++6-dev/
+# the rest are in libc6-dev
 mv *.ipk repack/libc6-dev/
 
-for dir in libc6 libc6-dev linux-libc-headers-dev; do
+for dir in libc6 libc6-dev linux-libc-headers-dev libgcc1 libgcc-dev libstdc\+\+6 libstdc\+\+6-dev; do
 	pushd repack/$dir
 		for file in *.ipk; do
 			ar x $file
@@ -41,10 +61,20 @@ find repack \( -name .install -o -name ..install.cmd \) -delete
 # we don't need arm binaries...
 rm repack/libc6/out/sbin/ldconfig
 rm repack/libc6/out/etc/ld.so.conf
+rm -rf repack/libc6-dev/out/sbin
+rm -rf repack/libc6-dev/out/usr/bin
+rm -rf repack/libc6-dev/out/usr/sbin
+rm -rf repack/libc6-dev/out/usr/libexec
 # remove all empty dirs (semi-recursive)
 rm -d repack/**/*(/^F)
 #rm -d repack/**/*(/^F)
 #rm -d repack/**/*(/^F)
+# move the arm-nilrt libs to arm-frcYEAR
+mv repack/libgcc-dev/out/usr/lib/arm-nilrt-linux-gnueabi repack/libgcc-dev/out/usr/lib/arm-frc${V_YEAR}-linux-gnueabi
+# copy the arm-nilrt headers to arm-frcYEAR
+# (we copy instead of move so gdb can find the originals)
+cp -Rp repack/libc6-dev/out/usr/lib/gcc/arm-nilrt-linux-gnueabi repack/libc6-dev/out/usr/lib/gcc/arm-frc${V_YEAR}-linux-gnueabi
+cp -Rp repack/libstdc++6-dev/out/usr/include/c++/6.3.0/arm-nilrt-linux-gnueabi repack/libstdc++6-dev/out/usr/include/c++/6.3.0/arm-frc${V_YEAR}-linux-gnueabi
 
 pushd repack/linux-libc-headers-dev/
 	mv out linux-libc-${Va_LINUX}
@@ -56,8 +86,21 @@ pushd repack/libc6/
 	mv ../libc6-dev/out libc6-${Va_LIBC}/libc6-dev
 	tar cjf "${THIS_DIR}/libc6-frc${V_YEAR}-armel-cross_${Va_LIBC}.orig.tar.bz2" * --owner=0 --group=0
 popd
+pushd repack/libgcc1/
+	mkdir libgcc1-${Va_LIBGCC}
+	mv out libgcc1-${Va_LIBGCC}/libgcc1
+	mv ../libgcc-dev/out libgcc1-${Va_LIBGCC}/libgcc-dev
+	tar cjf "${THIS_DIR}/libgcc1-frc${V_YEAR}-armel-cross_${Va_LIBGCC}.orig.tar.bz2" * --owner=0 --group=0
+popd
+pushd repack/libstdc++6/
+	mkdir libstdc++6-${Va_LIBSTDCPP}
+	mv out libstdc++6-${Va_LIBSTDCPP}/libstdc++6
+	mv ../libstdc++6-dev/out libstdc++6-${Va_LIBSTDCPP}/libstdc++6-dev
+	tar cjf "${THIS_DIR}/libstdc++6-frc${V_YEAR}-armel-cross_${Va_LIBSTDCPP}.orig.tar.bz2" * --owner=0 --group=0
+popd
 
 # Make frcmake tarball
+rm -rf frcmake${V_YEAR}-${V_FRCMAKE}
 mkdir frcmake${V_YEAR}-${V_FRCMAKE}
 pushd frcmake${V_YEAR}-${V_FRCMAKE} 
 sed -e "s/frc/frc${V_YEAR}/g" ../tools/frcmake > frcmake${V_YEAR}
